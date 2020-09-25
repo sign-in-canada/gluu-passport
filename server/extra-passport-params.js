518a4df19a9290ee8cdec538661566eee3c38a6b
Doug M. Harris <dharris@hq.xsilo.com>
2020-09-25 10:10:38 -0400

    More adjustments for scoped strategies

--- server/extra-passport-params.js
+++ server/extra-passport-params.js
@@ -8,7 +8,7 @@ const
 //This is wrapped in a function so params is not evaluated upon module load, only at first usage
 const params = R.once(() => [
 	{
-		strategy: 'sic-passport-saml',
+		strategy: '@sic/passport-saml',
 		verifyCallbackArity: 3,
 		passportAuthnParams: {},
 		options: {
