a67560aa7ddd0ef55d50dd42b9d8ad4cb032c252
Mahamoud <Mahamoud.Mohamoud@tbs-sct.gc.ca>
2023-07-20 15:50:43 +0000

    refactor: updated Jose to 4.8.1. Previous version of Jose renamed to v2 for debugging purposes

--- server/utils/openid-client-helper.js
+++ server/utils/openid-client-helper.js
@@ -1,13 +1,13 @@
-const { JWK: { generateSync, asKey }, JWKS: { KeyStore } } = require('jose')
+const { JWK: { generateSync, asKey }, JWKS: { KeyStore } } = require('jose_v2')
 const { Issuer } = require('openid-client')
 const path = require('path')
 const fs = require('fs')
 const fileUtils = require('../utils/file-utils')
 const { logger } = require('./logging')
 const clientJWKSFilePath = path.join(`${process.cwd()}/server`, 'jwks')
-const secretKey = require('./misc').secretKey();
-let ks = new KeyStore();
-const keysPath = "/run/keyvault/keys/";
+const secretKey = require('./misc').secretKey()
+let ks = new KeyStore()
+const keysPath = '/run/keyvault/keys/'
 
 /**
  * generate jwks and store it in file. file name will be like [provider.id].json
@@ -27,30 +27,30 @@ async function generateJWKS (provider) {
  * get keystore after creating and adding private keys
  * @returns keystore
  */
- async function getKeystore () {
+async function getKeystore () {
   logger.log('verbose', 'Importing private keys into the keystore')
 
   fs.readdirSync(keysPath).forEach(file => {
-    const akeyPath = path.resolve(keysPath, file);
+    const akeyPath = path.resolve(keysPath, file)
     if (fs.lstatSync(akeyPath).isFile()) {
-      const fileNameRegExp = /(.*?)_(.*?)_(.*?)\.pem/;
-      const matches = file.match(fileNameRegExp);
+      const fileNameRegExp = /(.*?)_(.*?)_(.*?)\.pem/
+      const matches = file.match(fileNameRegExp)
 
-      if (matches.length == 4) { // file naming: keydId_use_alg.pem
+      if (matches.length === 4) { // file naming: keydId_use_alg.pem
         try {
           const keyObj = {
             key: fs.readFileSync(akeyPath, 'utf8'),
             passphrase: secretKey
           }
-    
+
           const opts = {
             kid: `${matches[1]}_${matches[2]}_${matches[3]}`,
             use: matches[2],
             alg: matches[3].toUpperCase()
           }
-    
+
           // Create the key and add it to the keystore
-          const key = asKey(keyObj, opts)                    
+          const key = asKey(keyObj, opts)
           ks.add(key)
         } catch (err) {
           const msg = 'Private key was not successfully added to the keystore.'
@@ -58,7 +58,7 @@ async function generateJWKS (provider) {
         }
       }
     }
-  });
+  })
 
   return ks
 }
@@ -95,7 +95,7 @@ async function getClient (provider) {
   const issuer = await getIssuer(options)
   if (options.token_endpoint_auth_method && options.token_endpoint_auth_method === 'private_key_jwt' && options.use_request_object && options.use_request_object.toString() === 'true') {
     // getKeystore with private keys
-    if (ks.size == 0) ks = await getKeystore()
+    if (ks.size === 0) ks = await getKeystore()
     client = new issuer.Client(options, ks.toJWKS(true))
   } else {
     client = new issuer.Client(options)
